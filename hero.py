6b39b607be6507684a4588ebd00cff6c15694704
kcochran101 <cochran.kendall10@gmail.com>
2026-09-15 10:47:30 -0500

    hero.py
    completed hero part...named him bobby

--- hero.py
+++ hero.py
@@ -2,7 +2,19 @@ import random
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.health = 130
+        self.attack_power = 20
+    def attack(self):
+    # Return a random value from 1 through this Hero's attack power.
+        return random.randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        # Subtract damage, but do not allow health to fall below 0.
+        self.health = self.health - damage
+        if self.health <0:
+            self.health = 0
 
     def is_alive(self):
         # Return a Boolean based on this Hero's health. 
