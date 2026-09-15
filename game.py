992073ea8368e9686052affa7a26c7c49b892df2
kcochran101 <cochran.kendall10@gmail.com>
2026-09-15 10:47:05 -0500

    battle.py
    finished the hero and completed the battle

--- game.py
+++ game.py
@@ -1,8 +1,21 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Pink Potato"
 
+def battle(hero: Hero, enemy: Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+
+
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+    if hero.is_alive():
+        print(f"{hero.name} wins!")
+    else:
+        print(f"{enemy.name} wins!")
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -13,7 +26,9 @@ def main():
     goblin = Goblin("Gribble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    print("But no hero has answered the call... yet.")
+    bob = Hero("bobby")
+    print(f"{bob.name} enters the arena!")
+    battle(bob, goblin)
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -1,4 +1,9 @@
+import random
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
     pass
+
+    def is_alive(self):
+        # Return a Boolean based on this Hero's health. 
+        return self.health > 0
